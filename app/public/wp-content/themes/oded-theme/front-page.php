135b525d7850e9e66570062f82ffc2ff4bcf2cfd
OdeliaFink <odelia.finkel@gmail.com>
2024-11-30 17:52:18 -0500

    ficed footer, date, and added global variables

--- app/public/wp-content/themes/oded-theme/front-page.php
+++ app/public/wp-content/themes/oded-theme/front-page.php
@@ -7,11 +7,10 @@
   $current_month = date('M'); // Format: "Friday, October 6, 2023"
   $current_date = date('j'); // Format: "Friday, October 6, 2023"
   ?>
-  <div class="current-date"
-    style="text-align: right; margin-top: 10px; display: flex; flex-direction: row; justify-content: flex-end; align-items: flex-end;">
-    <h1 style="font-family: 'Rock 3D'; font-size: 100px; font-weight: 300; letter-spacing: -22px;">
-      <?php echo $current_date; ?></h1>
-    <p style="font-family: 'DM Serif Text'; margin-bottom: 1rem; text-transform: uppercase; font-size: 50px;">
+  <div class="current-date">
+    <p class="star">*</p>
+    <h1 class="date"><?php echo $current_date; ?></h1>
+    <p class="month">
       <?php echo $current_month; ?></p>
   </div>
   <div class="home-content">
