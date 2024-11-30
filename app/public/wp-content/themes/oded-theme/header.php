4808de640239c325f87f7983f17903ff1a421b58
OdeliaFink <odelia.finkel@gmail.com>
2024-11-30 16:13:37 -0500

    updated header and made responsive

--- app/public/wp-content/themes/oded-theme/header.php
+++ app/public/wp-content/themes/oded-theme/header.php
@@ -16,10 +16,12 @@
         </a>
         <p class="header-desc">CREATIVE WEB STUDIO</p>
       </div>
-      <svg id="hamburger" viewBox="0 0 100 100" width="40" height="40">
-        <rect y="45" width="100" height="10"></rect>
-        <rect x="45" width="10" height="100"></rect>
+      <svg id="hamburger" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#9FB99F"
+          d="M30.3,-25.6C46.4,-14.2,71.5,-7.1,73.2,1.7C74.9,10.5,53.3,21.1,37.2,25.5C21.1,30,10.5,28.3,-2.2,30.5C-14.8,32.6,-29.7,38.6,-36.9,34.1C-44.1,29.7,-43.8,14.8,-42.6,1.2C-41.4,-12.5,-39.4,-24.9,-32.1,-36.3C-24.9,-47.7,-12.5,-58,-2.7,-55.3C7.1,-52.6,14.2,-37,30.3,-25.6Z"
+          transform="translate(100 100)" />
       </svg>
+
       <nav id="menu">
         <?php
         wp_nav_menu(array(
