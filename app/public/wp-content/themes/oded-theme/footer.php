9f42c5c942db676a10ba3aa8b137f34d8b586655
OdeliaFink <odelia.finkel@gmail.com>
2024-11-30 16:28:10 -0500

    added date and modified section css

--- app/public/wp-content/themes/oded-theme/footer.php
+++ app/public/wp-content/themes/oded-theme/footer.php
@@ -1,22 +1,23 @@
 <footer class="site-footer">
+  <section>
+    <div style="display: flex;flex-direction: row; justify-content: space-between; align-items:center;
+   margin-top: 9px;">
 
-  <div style="display: flex;flex-direction: row; justify-content: space-between; align-items:center; width: 90%;
-  margin-inline: auto; margin-top: 9px;">
-
-    <div style="display: flex;flex-direction: row; gap: 2rem;">
-      <p>email</p>
-      <p>linkedin</p>
+      <div style="display: flex;flex-direction: row; gap: 2rem;">
+        <p>email</p>
+        <p>linkedin</p>
+      </div>
+      <div class="footer-logo">
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+          <h1>ODED</h1>
+        </a>
+      </div>
     </div>
-    <div class="footer-logo">
-      <a href="<?php echo esc_url(home_url('/')); ?>">
-        <h1>ODED</h1>
-      </a>
-    </div>
-  </div>
-  <div class="footer-content">
-    <p>&copy; <?php echo date('Y'); ?> ODED STUDIOS</p>
+    <div class="footer-content">
+      <p>&copy; <?php echo date('Y'); ?> ODED STUDIOS</p>
 
-  </div>
+    </div>
+  </section>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- app/public/wp-content/themes/oded-theme/front-page.php
+++ app/public/wp-content/themes/oded-theme/front-page.php
@@ -2,7 +2,18 @@
 
 <section>
   <div id="react-root"></div>
-
+  <?php
+  // Get the current date
+  $current_month = date('M'); // Format: "Friday, October 6, 2023"
+  $current_date = date('j'); // Format: "Friday, October 6, 2023"
+  ?>
+  <div class="current-date"
+    style="text-align: right; margin-top: 10px; display: flex; flex-direction: row; justify-content: flex-end; align-items: flex-end;">
+    <h1 style="font-family: 'Rock 3D'; font-size: 100px; font-weight: 300; letter-spacing: -22px;">
+      <?php echo $current_date; ?></h1>
+    <p style="font-family: 'DM Serif Text'; margin-bottom: 1rem; text-transform: uppercase; font-size: 50px;">
+      <?php echo $current_month; ?></p>
+  </div>
   <div class="home-content">
     <h1>Welcome to Oded Studios</h1>
     <p>We specialize in creative design and development.</p>
