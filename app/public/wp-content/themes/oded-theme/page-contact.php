8d43f79ae933e4fbfc1966b2e5b5231642cbd8e1
OdeliaFink <odelia.finkel@gmail.com>
2024-12-11 22:03:28 -0500

    started base for contact

---
+++ app/public/wp-content/themes/oded-theme/page-contact.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<section>
+  <div style="max-width: 70%; margin-inline: auto; text-align: center; margin-bottom: 2rem;">
+    <h1>SEND AN EMAIL BY CLICKING ONE OF THE TOPIC BUBBLES BELOW</h1>
+  </div>
+  <div class="button-container" style="gap: 4rem; overflow-x: none;">
+    <a href="mailto:odelia.finkel@gmail.com?subject=Web inquiry: DIGITAL CONSULTANT" target="_blank"
+      class="contact-button">DIGITAL
+      CONSULTANT</a>
+    <a href="mailto:odelia.finkel@gmail.com?subject=Web inquiry: UX/UI DESIGN" target="_blank"
+      class="contact-button">UX/UI DESIGN</a>
+    <a class="contact-button">FRONTEND DEVELOPMENT</a>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
